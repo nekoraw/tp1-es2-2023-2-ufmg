b4a08a1a1a7a8f34da4ca3eef83de164ae576d37
nekoraw <michelbarros123456@gmail.com>
2023-11-03 20:20:01 -0300

    remoção de código morto

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,3 @@ origins = ["http://localhost:5173"]
 app.add_middleware(
     CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
 )
-
-
-@app.get("/")
-def root():
-    return {"Hello": "World"}
